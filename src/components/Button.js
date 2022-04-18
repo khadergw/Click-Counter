3e2bdc07bd2f4555c9cb5c7c6b62aab52a1871fd
Ghadeer Khader <qods_a@yahoo.com>
2022-04-18 00:18:36 -0400

    adding counts

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Button = () => {
+  const [CurrentCount, setCurrentCount] = useState(0);
+
+  const handleClick = () => {
+    setCurrentCount(CurrentCount+1)
+  }
+
   return <div>
-  <button>Plus 1</button>
+  <button onClick={handleClick}>Plus 1</button>
+
+<div>{CurrentCount}</div>
+
   </div>
+
 }
 
 export default Button
