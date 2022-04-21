1d66ec89228ad3ea1ccf042a6521ea63ea88b0a3
Ghadeer Khader <qods_a@yahoo.com>
2022-04-20 23:59:55 -0400

    inline styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,10 +7,38 @@ const Button = () => {
     setCurrentCount(CurrentCount+1)
   }
 
-  return <div>
-  <button onClick={handleClick}>Plus 1</button>
+ 
+  const ParentStyle = {
+    textAlign: "center"
+  }
 
-<div>{CurrentCount}</div>
+  const ButtonStyle = {
+    backgroundColor:"red",
+    color: "white",
+    fontSize: "30px",
+    fontWeight: "bolder",
+    height: "150px",
+    width: "150px",
+    borderRadius: "50%",
+    border: "5px solid black",
+    marginTop: "5%",
+    cursor: "pointer",
+    boxShadow: "7px 3px gray" 
+
+  }
+
+  const DisplayStyle = {
+    color: "black",
+    fontSize: "35px",
+    fontWeight: "bolder",
+    marginTop: "5%"
+  }
+
+
+  return <div style={ParentStyle}>
+  <button style={ButtonStyle} onClick={handleClick}>Plus 1</button>
+
+<div style={DisplayStyle}>{CurrentCount}</div>
 
   </div>
 
